0de4b313125e1be4edfde8880d19eeecc1c7fe8f
Eva Yu <eva.yw.yu@gmail.com>
2015-11-07 16:35:15 -0800

    Added to the grid and square classes in the grid package

--- grid/Grid.java
+++ grid/Grid.java
@@ -11,27 +11,55 @@ package assignment2.grid;
  */
 public class Grid {
 
-    int row;
-    int colummn;
-
+    private int row;
+    private int colummn;
+    /**
+     * this constructor sets a defaukt 10 by 10 grid
+     */
     public Grid() {
-        row = 10;
-        colummn = 10;
+        this.row = 10;
+        this.colummn = 10;
     }
-
+    /**
+     * this constructor sets the sqaure grid of an indicated size  
+     * @param  size [description]
+     */
+    public Grid(int size){
+        this.row = size;
+        this.colummn = size;
+    }
+    /**
+     * this constructor sets a grid with r rows and c columns
+     * @param  r the number of rows in the grid 
+     * @param  c the number of columns in the grid 
+     */
     public Grid(int r, int c) {
-        colummn = c;
-        row = r;
+        this.row = r;
+        this.colummn = c;
     }
-
+    /**
+     * gets the number of rows there are in the grid 
+     * @return int indicating number of rows in grid 
+     */
     public int getRow() {
         return this.row;
     }
-
+    /**
+     * returns number of columns found in the row 
+     * @return int indicating number of cols in grid 
+     */
     public int getCol() {
         return this.colummn;
     }
-
+    /**
+     * gets the numeric value of the position of the graph according to the x and y indices
+     * numbered from 1 - n 
+     * from left to right, top to bottom 
+     * 
+     * @param  x [int indicating the row ]
+     * @param  y [int indicating the col ]
+     * @return   [int indicating the numberic value assinged to the intersetion of the x an y indices]
+     */
     public int getPos(int x, int y) {
         return (x * col) + y + 1;
     }
--- grid/Square.java
+++ grid/Square.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+NOTE TO SELF: Still need to fix the boundaries of the left, right upper, lower in a grid!!! 
  */
 package assignment2.grid;
 
@@ -20,9 +18,11 @@ public class Square {
     			  //going from left to right, 
     			  //top to bottom 
     			  //startin at 1;
+    
     protected int prevSquare; // for making the square a node in a linked list 
     protected int nextSquare; // for making the square a node in a linked list 
 
+    /* the following variables will needs to be assocaited with the grid class*/
     protected int rightSquare; // for making the square a node in a graph
     protected int leftSquare; // for making the square a node in a graph
     protected int lowerSquare; // for making the square a node in a graph
@@ -53,15 +53,15 @@ public class Square {
      * @param  s   [takes in the length of a side of the square]
      * @param  pos [description]
      */
-    public Square(int s, int pos){
+    public Square(int s, int pos, Grid grid){
     	this.square(s);
-    	this.setPosition(pos);
+    	this.setPosition(pos, grid);
 	}
 	/**
      */
     /**
-     * this constructor is for a sqaure that is part of a grid or map
-     * it goes to link the squares with each other as well
+     * this constructor is for a sqaure that is part of a linked list
+     * it goes to link the squares with each other and 
      * it will set the position of the square
      *  
      * @param  s    [takes in the lenght of a side of a square]
@@ -79,8 +79,14 @@ public class Square {
 	 * Finds the position of the square on  a linked list or map 
 	 * @param pos [description]
 	 */
-	protected void setPosition(int pos){
+	protected void setPosition(int pos, Grid grid){
+		int x = grid.getCol(); // retrives number the rows in the grid 
+		int y = grid.getRow(); // retrieves the the columns in the grid 
 		this.position = pos;
+		setrightSquare(x,y);
+		setupperSquare(x,y);
+		setlowerSquare(x,y);
+		setLeftSquare(x,y);
 	}
 	/**
 	 * [getPosition description]
@@ -89,22 +95,69 @@ public class Square {
 	public int getPosition(){
 		return this.position;
 	}
-	
-	protected void setLeftSquare()
-	
+	/**
+	 * sets the position of the sqaure left of the current square on the grid
+	 * @param x [int indicating the number of rows]
+	 * @param y [int indicating the number of columns]
+	 */
+	protected void setLeftSquare(int x , int y){
+		if(){
+
+		}
+		this.leftSquare = this.position - 1;
+	}
+	/**
+	 * set the position of the square right of the current square on the grid 
+	 * @param x [int indicating the number of rows]
+	 * @param y [int indicating the number of columns]
+	 */
+	protected void setRightSquare(int x , int y){
+		this.rightSquare = this.position + 1;
+	}
+	/**
+	 * set the position of the square on top of the current square on the grid 
+	 * @param x [int indicating the number of rows]
+	 * @param y [int indicating the number of columns]
+	 */
+	protected void setUpperSquare(int x , int y){
+		this.upperSquare = this.position - col;
+	}
+	/**
+	 * sets the position of the sqaure below the current square on the grid
+	 * @param x [int indicating the number of rows]
+	 * @param y [int indicating the number of columns]
+	 */
+	protected void setLowerSquare(int x , int y){
+		this.lowerSquare = this.position + col;
 	}
 
-	protected void setLeftSquare()
-	
+	/**
+	 * returns the position of the sqaure on the left of the current sqaure 
+	 * @return [in representing the numerical position of the square left of the current square]
+	 */
+	public int getLeftSquare(){
+		return this.leftSquare; 
 	}
-	
-	protected void setLeftSquare()
-	
+	/**
+	 * returns the position of the sqaure on the right of the current sqaure 
+	 * @return [in representing the numerical position of the square right of the current square]
+	 */
+	public int getRightSquare(){
+		return this.rightSquare; 
 	}
-	
-	protected void setLeftSquare()
-	
+	/**
+	 * returns the position of the sqaure on the top of the current sqaure 
+	 * @return [in representing the numerical position of the square on top of the current square]
+	 */
+	public int getUpperSquare(){
+		return this.upperSquare; 
 	}
-
+	/**
+	 * returns the position of the sqaure the underneath of the current sqaure 
+	 * @return [in representing the numerical position of the square underneath the current square]
+	 */
+	public int getLowerSquare(){
+		return this.lowerSquare; 
+	}
 
 }
